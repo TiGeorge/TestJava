487709a2196817bc67aeeb2f1ab255dabbd85116
TiGeorge <georgetikhomirov@mail.ru>
2020-04-30 07:40:34 +0300

    commit

---
+++ src/_Bloch/part3/Point.java
@@ -0,0 +1,49 @@
+package _Bloch.part3;
+
+import java.awt.*;
+
+public class Point {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if(!(obj instanceof Point)) return false;
+        Point p = (Point) obj;
+        return p.x == x && p.y == y;
+    }
+}
+
+class ColorPoint extends Point {
+    private final Color color;
+
+    public ColorPoint(int x, int y, Color color) {
+        super(x, y);
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Point)) return false;
+        if(!(obj instanceof ColorPoint)) return super.equals(obj);
+        return super.equals(obj) && ((ColorPoint) obj).color == color;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        ColorPoint p1 = new ColorPoint(1, 2, Color.RED);
+        Point p2 = new Point(1, 2);
+        ColorPoint p3 = new ColorPoint(1, 2, Color.BLUE);
+        System.out.println(p1.equals(p2));
+        System.out.println(p2.equals(p3));
+        System.out.println(p1.equals(p3));
+
+    }
+}
